7bee4f31b4dc6c3a2a4ee1b435b0a4f11df2faf1
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-09-02 10:44:59 +0530

    removed google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
-
 import Portfolio from "@/components/portfolio";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect } from "react";
-import { analytics } from "@/lib/firebaseConfig"; // Adjust the import path accordingly
-import { logEvent } from "firebase/analytics";
+
 
 export default function Home() {
-  useEffect(() => {
-    if (analytics) {
-      logEvent(analytics, "page_view");
-    }
-  }, []);
 
   return (
     <div className="">
