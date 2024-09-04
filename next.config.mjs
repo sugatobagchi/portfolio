dc943d3b4e410331d8bc7cfb422271ce7d981d6c
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-09-04 22:35:25 +0530

    meet route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: async () => {
+        const redirectsArray = [];
+
+        for (let i = 1; i <= 9; i++) {
+            redirectsArray.push({
+                source: `/meet${i}`,
+                destination: `https://meet.google.com/ojg-fjjb-hin?authuser=${i}`,
+                permanent: true,
+            });
+        }
+
+        redirectsArray.push({
+            source: '/meet',
+            destination: 'https://meet.google.com/ojg-fjjb-hin',
+            permanent: true,
+        });
+
+        redirectsArray.push({
+            source: '/meet:authuser(\\d+)', 
+            destination: 'https://meet.google.com/ojg-fjjb-hin', 
+            permanent: true,
+        });
+
+        return redirectsArray;
+    },
+};
 
 export default nextConfig;
